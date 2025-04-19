9a3e90a32a3e78807cbc6cc63ee327e57e945452
Otech <otengebenezer326@gmail.com>
2025-04-19 03:11:07 -0700

    Update AllSneakers.jsx

--- Client/src/Components/AllSneakers.jsx
+++ Client/src/Components/AllSneakers.jsx
@@ -201,7 +201,7 @@ const AllSneakers = ({ products, updateProducts, resetProducts, originalProducts
                       onClick={() => handleImageClick(item)}
                       src={item.img?.startsWith("http") ? item.img : item.imageUrl || "https://via.placeholder.com/150"}
                       alt={item.name}
-                      className="w-full h-full object-cover cursor-pointer"
+                      className="w-40 h-40 md:w-full md:h-full lg:w-full lg:h-full object-cover cursor-pointer"
                     />
                     <div className="absolute top-4 left-6 flex flex-col items-center">
                       <div className="flex items-center space-x-2">
